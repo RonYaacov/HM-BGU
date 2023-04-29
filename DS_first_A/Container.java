659aeafa16486f83f1b2db3c391105e96ae432c9
Ron Yaacov <ronyaacov19@gamil.com>
2023-04-29 20:01:00 +0300

    create container

--- DS_first_A/Container.java
+++ DS_first_A/Container.java
@@ -2,10 +2,20 @@
 //Don't change the class name
 public class Container{
 	private Point data;//Don't delete or change this field;
+	private YNode yNode;
+
+	public Container(Point p, YNode yNode){
+		this.data = p;
+		this.yNode = yNode;
+	}
 	
 	//Don't delete or change this function
 	public Point getData()
 	{
 		return data;
+	}
+
+	public YNode getYNode(){
+		return yNode;
 	}
 }
