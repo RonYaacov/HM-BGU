598e19447545fdbe79dbfeee1e2363d2c4c4ab9a
Ron Yaacov <ronyaacov19@gamil.com>
2023-06-03 15:45:58 +0300

    .

--- Second_Semester/DS_second_A/StringHash.java
+++ Second_Semester/DS_second_A/StringHash.java
@@ -1,24 +1,57 @@
 import java.util.Random;
 
+
 public class StringHash implements HashFactory<String> {
+    private int q;
+    private int c;
+    private HashingUtils utils;
+
 
     public StringHash() {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+        utils = new HashingUtils();
+        assignRandFields();    
+    }
+    private void assignRandFields(){
+        Random rand  = new Random();
+        boolean isPrime = false;
+        while(!isPrime){
+            q = (int)utils.genLong(Integer.MAX_VALUE/2+1, Integer.MAX_VALUE+1);
+            isPrime = utils.runMillerRabinTest(q, 30);
+        }
+        c = rand.nextInt(q-2)+2;
     }
 
     @Override
     public HashFunctor<String> pickHash(int k) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+        HashFunctor<Integer> carterWegman = new ModularHash().pickHash(k);
+        Functor result = new Functor(q,c, carterWegman);// in the descrption k is always valid 
+        assignRandFields();
+        return result;
+
     }
 
     public class Functor implements HashFunctor<String> {
         final private HashFunctor<Integer> carterWegmanHash;
         final private int c;
         final private int q;
+        
+        public Functor(int q, int c, HashFunctor<Integer> carterWegman ){
+            this.q = q;
+            this.c = c;
+            this.carterWegmanHash = carterWegman;
+
+        } 
 
         @Override
         public int hash(String key) {
-            throw new UnsupportedOperationException("Replace this by your implementation");
+            int sum = 0;
+            char[] arr = key.toCharArray();
+            for(int i=1; i<=arr.length;i++){
+                long cPow = HashingUtils.fastModularPower((long)c, (long)(arr.length-i), (long)q);
+                long innerMod = HashingUtils.mod(arr[i-1]*cPow,q);
+                sum += HashingUtils.mod(innerMod, q);
+            }
+            return carterWegmanHash.hash(sum);
         }
 
         public int c() {
