1815a081dca3a7477104206671ce07bb61cc7785
Ron Yaacov <ronyaacov19@gamil.com>
2023-06-13 16:16:44 +0300

    .

--- Second_Semester/OOP-3/src/com/company/Enemy.java
+++ Second_Semester/OOP-3/src/com/company/Enemy.java
@@ -11,7 +11,6 @@ public abstract class Enemy implements Visitor {
     public int getExperienceReword(){
         return experienceReword;
     }
-     
 
     @Override
     public void visitBattle(Player player){
---
+++ Second_Semester/OOP-3/src/com/company/Monster.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Monster extends Enemy {
+
+    public Monster(int experienceReword) {
+        super(experienceReword);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
