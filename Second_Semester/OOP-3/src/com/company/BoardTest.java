98617ac82d197248f9d131811a0027768d7fb162
Escobachar <bacyuv@bgu.ac.il>
2023-06-27 20:35:49 +0300

    .

---
+++ Second_Semester/OOP-3/src/com/company/BoardTest.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class BoardTest {
+    public static void main(String[] args){
+        Board board = new Board("/Users/yuvalbachar/Documents/uni/Year A/Semester B/OOP/assignment 4/Second_Semester/OOP-3/src/com/company/levels_dir/level1.txt", "Jon Snow");
+        System.out.println(board.toString());
+    }
+}
