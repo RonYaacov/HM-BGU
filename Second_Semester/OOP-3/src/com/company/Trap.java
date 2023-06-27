5ff0232605f7d295d80138da72bfb5e13faad554
Ron Yaacov <ronyaacov19@gamil.com>
2023-06-27 20:00:23 +0300

    .

--- Second_Semester/OOP-3/src/com/company/Trap.java
+++ Second_Semester/OOP-3/src/com/company/Trap.java
@@ -1,20 +1,30 @@
 package com.company;
 
-import java.beans.Visibility;
 
-public class Trap {
+import com.company.Interfaces.TickListener;
+
+public class Trap implements TickListener {
     private int visibilityTime;
     private int invisibilityTime;
     private int ticksCount;
     private boolean isVisible;
 
-    public Trap(int visibilityTime, int invisibilityTime) {
+    public Trap(int visibilityTime, int invisibilityTime){
         this.visibilityTime = visibilityTime;
         this.invisibilityTime = invisibilityTime;
         this.ticksCount = 0;
         this.isVisible = true;
     }
-    
-    
 
+    @Override
+    public void tick() {
+        ticksCount++;
+        if(ticksCount == visibilityTime){
+            isVisible = false;
+        }
+        else if(ticksCount == visibilityTime + invisibilityTime){
+            isVisible = true;
+            ticksCount = 0;
+        }
+    }
 }
