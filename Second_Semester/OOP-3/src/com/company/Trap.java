a72664c2baa2d132aedae294c8bb192712baedc9
Ron Yaacov <ronyaacov19@gamil.com>
2023-06-27 17:51:51 +0300

    .

--- Second_Semester/OOP-3/src/com/company/Trap.java
+++ Second_Semester/OOP-3/src/com/company/Trap.java
@@ -1,5 +1,20 @@
 package com.company;
 
+import java.beans.Visibility;
+
 public class Trap {
+    private int visibilityTime;
+    private int invisibilityTime;
+    private int ticksCount;
+    private boolean isVisible;
+
+    public Trap(int visibilityTime, int invisibilityTime) {
+        this.visibilityTime = visibilityTime;
+        this.invisibilityTime = invisibilityTime;
+        this.ticksCount = 0;
+        this.isVisible = true;
+    }
     
+    
+
 }
