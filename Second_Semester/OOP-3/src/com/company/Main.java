a8fd8344a62db0a7efc187319674db94273889f6
Ron Yaacov <ronyaacov19@gamil.com>
2023-08-08 18:04:13 +0300

    .

--- Second_Semester/OOP-3/src/com/company/Main.java
+++ Second_Semester/OOP-3/src/com/company/Main.java
@@ -4,10 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         UI ui = new UI();
-        
-        GameManeger gameMangManeger = new GameManeger(System.getProperty("user.dir")+"\\level1.txt");
+        GameManeger gameMangManeger = new GameManeger(System.getProperty("user.dir")+"\\Second_Semester\\OOP-3\\src\\com\\company\\levels_dir\\level1.txt");
         ui.Register(gameMangManeger);
         ui.getPlayerType();
+        ui.printBoard(gameMangManeger.getBoard());
+        
 
     }
 }
