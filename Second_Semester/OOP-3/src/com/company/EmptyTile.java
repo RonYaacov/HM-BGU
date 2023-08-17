e24f42360adc1a60dd02bef92470f9972236e173
Ron Yaacov <ronyaacov19@gamil.com>
2023-08-17 13:24:38 +0300

    F

--- Second_Semester/OOP-3/src/com/company/EmptyTile.java
+++ Second_Semester/OOP-3/src/com/company/EmptyTile.java
@@ -1,11 +1,14 @@
 package com.company;
 
+import com.company.enums.TileType;
+import com.company.Interfaces.Visitor;
+
 public class EmptyTile extends Tile {
     public EmptyTile(){
         super('.');
     }
     @Override
-    public String visitPosChanged(Board board){
+    public TileType visitPosChanged(Board board){
         return board.visitMove(this);
     }
     public void setPosition(Position position){
@@ -13,5 +16,17 @@ public class EmptyTile extends Tile {
     }
     public Position getPosition(){
         return position;
+    }
+    @Override
+    public void acceptBattle(Visitor visitor) {
+        return;
+    }
+    @Override
+    public void visitBattle(Player player) {
+        return;
+   }
+    @Override
+    public void visitBattle(Enemy enemy) {
+        return;
     }
 }
--- Second_Semester/OOP-3/src/com/company/Wall.java
+++ Second_Semester/OOP-3/src/com/company/Wall.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import com.company.Interfaces.Visitor;
+
 public class Wall extends Tile{
     public Wall(){
         super('#');
+    }
+
+    @Override
+    public void acceptBattle(Visitor visitor) {
+        
+        return;
+    }
+
+    @Override
+    public void visitBattle(Player player) {
+        return;    
+    }
+
+    @Override
+    public void visitBattle(Enemy enemy) {
+        return;    
     }
 
 }
