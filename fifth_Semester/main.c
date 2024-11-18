48824c5f61b9a332b17d265af9f70444dade2341
Ron Yaacov <ronyaacov19@gmail.com>
2024-11-18 19:38:43 +0200

    task 1 done

--- fifth_Semester/main.c
+++ fifth_Semester/main.c
@@ -1,4 +1,41 @@
 
-int main(){
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+FILE *infile;
+FILE *outfile;
+
+char *encoder(char *input){
+    return input;
+}
+
+void program_loop(){
+    char c;
+    while((c = fgetc(infile)) != EOF){
+        fputs(encoder(&c), stdout);
+    }
+}
+
+
+int main(int argc, char *argv[]) {    
+    infile = stdin;
+    outfile = stderr;
+    bool debug = true;
+    for(int i=1; i<argc; i++){
+        if(strcmp(argv[i], "+D") == 0){
+            debug = true;        
+            continue;
+        }
+        if(strcmp(argv[i], "-D") == 0){
+            debug = false;
+            continue;
+        }
+        if(debug){
+            fprintf(outfile, "%s\n",argv[i]);
+        }
+    }
+    program_loop();
+
     return 0;
 }
