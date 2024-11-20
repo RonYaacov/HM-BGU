bb52af6ad3ef766eccbd23aa8d68391b1f0497ae
Ron Yaacov <ronyaacov19@gmail.com>
2024-11-20 16:08:04 +0200

    have mod and have pow with wrong order

--- fifth_Semester/comp/hw1.ml
+++ fifth_Semester/comp/hw1.ml
@@ -142,8 +142,8 @@ and nt_expr0 str =
 and nt_expr1 str = 
   let nt1 = pack (char '*') (fun _ -> Mul) in
   let nt2 = pack (char '/') (fun _ -> Div) in
-  (* let nt3 = pack (char '%') (fun _ -> Mod) in *)
-  let nt1 = disj nt1 nt2 in
+  let nt3 = pack (word "mod") (fun _ -> Mod) in
+  let nt1 = disj_list [nt1; nt2; nt3] in
   let nt1 = star (caten nt1 nt_expr2) in 
   let nt1  = pack (caten nt_expr2 nt1) (fun (expr2, binop_exprlst) -> 
     List.fold_left (fun expr2 (binop, expr2') -> BinOp(binop, expr2, expr2')) expr2 binop_exprlst) in
