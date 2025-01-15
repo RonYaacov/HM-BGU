8aaad07b5ed0af1edccc8b9226659f661d3a11a6
Ron Yaacov <ronyaacov19@gmail.com>
2025-01-15 10:25:22 +0200

    task1b work in progess

--- fifth_Semester/Lab/LabD/multi.asm
+++ fifth_Semester/Lab/LabD/multi.asm
@@ -1,39 +1,64 @@
+global main
+extern printf
+extern fgets
+extern stdin
+
 section .data
 format_byte db "%02hhx", 10, 0
 format_word db "%04hx", 0
 end_of_line db 10, 0
 debug_msg_string db "Debug: %s", 10, 0
 debug_msg_int db "Debug: %d", 10, 0
-x_struct dw 5
-x_num dw 0x00aa, 1,2,0x0044,0x004f
+x_struct dw 0
+
+section .bss
+x_num resb 600
+x_buffer resb 600
 
 section .text
-global main
-extern printf
-
 
 main:
     pusha
-    mov eax, [x_struct]
-    and eax, 0xFFFF ; Zero-extend the word to dword
-    push x_num
-    push eax
-    call print_multi 
-    add esp, 8 ;clean the stack
+    call get_multi
+    ;mov eax, [x_struct]
+    ;and eax, 0xFFFF ; Zero-extend the word to dword
+    ;push x_num
+    ;push eax
+    ;call print_multi 
+    ;add esp, 8 ;clean the stack
     popa
     ret
 
-print_multi:
+get_multi:
+    push ebp
+    mov ebp, esp
+    pusha
+    push dword [stdin]
+    push dword 600
+    push dword x_buffer
+    call fgets
+    add esp, 12 ;clean the stack
+    push x_buffer
+    push 1
+    call print_multi
+    add esp, 8 ;clean the stack
+    popa
+    pop ebp
+    ret
+
+
+
+print_multi: ;(x_size, x_num_array address)
     push ebp
     mov ebp, esp   
     pusha
-    mov eax, [ebp + 8] ; x_struct
+    mov eax, [ebp + 8] ; x_struct size
     mov ebx, [ebp + 8 + 4 ] ; address of x_num
     .print_loop:
         cmp eax, 0 ;check if we reached the end of the struct
         je .end_print_loop ;if so, end the loop
         ;print the argument
-        mov esi, [ebx + 2*eax - 2]
+        mov esi, [ebx + 2*eax - 2]; get the word from the array x_num
         pusha
         push esi
         push format_word
