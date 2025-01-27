5551864a50264ff5ac4168ecc916e203f4e1c3bd
Ron Yaacov <ronyaacov19@gmail.com>
2025-01-27 11:30:25 +0200

    test 00 done

---
+++ fifth_Semester/comp/compFinal/testing/our_test.scm
@@ -0,0 +1,11 @@
+(define free_var 2)
+
+(define tail_lambda
+    (lambda (x f) (f x))
+)
+(define arg_lambda
+    (lambda (x) x)
+)
+
+(define free_var_lambda (lambda () free_var))
+(free_var_lambda)
